3d9ec299c434095c26cfc36ac984e6b83a606f4d
Steven J Bark <stevenjbark@gmail.com>
2019-10-21 10:03:06 -0500

    Fourth Commit: 98% On PyBank and PyPoll Script, Printing in Place, Need Final Formatting

--- PythonScripts/mainpollscript.py
+++ PythonScripts/mainpollscript.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 #Setup the file path for opening the election_test.csv file.
-path = os.path.join('Resources', 'election_test.csv')
+path = os.path.join('Resources', 'election_data.csv')
 
 #Open the election_test.csv file using with statement
 with open(path, 'r', newline='') as efile:
@@ -10,6 +10,7 @@ with open(path, 'r', newline='') as efile:
 #Use csv to generate a reader object for analysis. Save header
     csvreader = csv.reader(efile, delimiter = ',')
     csv_header = next(csvreader)
+    print(csv_header)
     
 #As observed in the other PyBank file, the with option for opening files
 #closes the file after use. Read the file contents into a list of lists for
@@ -61,10 +62,10 @@ totalvotes = K + C + L + O
 Khanpercent = round((K / totalvotes) * 100, 2)
 Correypercent = round((C / totalvotes) * 100, 2)
 Lipercent = round((L / totalvotes) * 100, 2)
-OToolepercent = round((O / totalvotes) * 100, 2)
+OTooleypercent = round((O / totalvotes) * 100, 2)
 
 #Comparison between the percentages of votes for each candidate.
-winner = [Khanpercent, Correypercent, Lipercent, OToolepercent]
+winner = [Khanpercent, Correypercent, Lipercent, OTooleypercent]
 winner.sort()
 
 #Calculate final winner
@@ -82,19 +83,30 @@ elif winner[3] == Lipercent:
         final.append("Li")
         final.append(Lipercent)
     
-elif winner[3] == OToolepercent: 
+elif winner[3] == OTooleypercent: 
         final.append("O'Toole")
         final.append(OToolepercent)
 
-
+#Print final results from data.
 print("---------------------------------------------")        
-print("Election Results Just In From All Precints!")
+print(f"Election Results Just In From All Precints with {totalvotes} Votes Cast!")
 print("---------------------------------------------")       
 print(f"Khan received {K} votes. This was {Khanpercent} percent of all votes cast.")
 print(f"Correy received {C} votes.This was {Correypercent} percent of all votes cast.")
 print(f"Li received {L} votes.This was {Lipercent} percent of all votes cast.")
-print(f"O'Toole received {O} votes. This was {OToolepercent} percent of all votes cast.")
+print(f"O'Tooley received {O} votes. This was {OTooleypercent} percent of all votes cast.")
 print("---------------------------------------------")
-print(f"The Winner: {final[0]} with {final[1]} percent of the vote.")
+print(f"The Official-elect: {final[0]} with {final[1]} percent of the vote.")
         
-
+#Setup and print file with these data using csvwriter object.
+with open('ElectionResults.csv', 'w', newline='') as ER:  
+    csvwriter = csv.writer(ER, delimiter = ',')
+    csvwriter.writerow([1, (f"Election Results Just In From All Precints with {totalvotes} Votes Cast!")])
+    csvwriter.writerow([2, ("---------------------------------------------")])
+    csvwriter.writerow([3, (f"Khan received {K} votes. This was {Khanpercent} percent of all votes cast.")])
+    csvwriter.writerow([4, (f"Correy received {C} votes.This was {Correypercent} percent of all votes cast.")])
+    csvwriter.writerow([5, (f"Li received {L} votes.This was {Lipercent} percent of all votes cast.")])
+    csvwriter.writerow([6, (f"O'Tooley received {O} votes. This was {OTooleypercent} percent of all votes cast.")])
+    csvwriter.writerow([7, ("---------------------------------------------")])
+    csvwriter.writerow([8, (f"The Official-elect: {final[0]} with {final[1]} percent of the vote.")])
+    
